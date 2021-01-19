47bc2a1f70e44c54819ffa3ca8bfcd7642a93a58
Anthony <denicad@msn.com>
2021-01-19 15:44:20 -0500

    board created. working on snake movement

--- test.js
+++ test.js
@@ -1,10 +1,24 @@
-const data = `<td>2</td>`
- document.querySelector('.board').insertAdjacentHTML('afterbegin', `<tbody></tbody>`);
-    const body= document.querySelector('.board').firstChild;
-for (let i = 0; i < 20; i++) {
-//    document.querySelector(`.board`).insertAdjacentHTML('afterbegin', `<tr>1${data.repeat(20)}</tr>`);
-   body.insertAdjacentHTML('afterbegin', `<tr>${data.repeat(20)}</tr>`);
-//    document.querySelector('.board').insertAdjacentHTML('afterbegin', `<tr>${data.repeat(20)}</tr>`);
-    
-    
-}
+const boardCreation=function(){
+    document.querySelector('.board').insertAdjacentHTML('afterbegin', `<tbody></tbody>`);
+    const body = document.querySelector('.board').firstChild;
+    for (let i = 0; i < 20; i++) {
+        const rowClass = `tr-${i+1}`;
+        const data = `<tr class=${rowClass}></tr>`;
+        body.insertAdjacentHTML('afterbegin', data);
+        for (let j = 0; j < 20; j++) {
+            const columnClass = `td-${j+1}`;
+            const data = `<td class=${columnClass}>2</td>`;
+            document.querySelector(`.${rowClass}`).insertAdjacentHTML(`afterbegin`, data);
+        }
+
+
+    };
+};
+boardCreation();
+const snake = {
+    head: [20, 1],
+
+};
+console.log(document.querySelector(`.tr-${snake.head[0]}`));
+document.querySelector(`.td-${snake.head[0]}`).classList.add(`taken`);
+
