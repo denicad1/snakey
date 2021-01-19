d6a4eea5f14bd9c376e948bd1c720a773caa8cef
Anthony <denicad@msn.com>
2021-01-19 17:22:46 -0500

    left right movement done

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const boardCreation=function(){
+const boardCreation = function () {
     document.querySelector('.board').insertAdjacentHTML('afterbegin', `<tbody></tbody>`);
     const body = document.querySelector('.board').firstChild;
     for (let i = 0; i < 20; i++) {
@@ -10,14 +10,39 @@ const boardCreation=function(){
             const data = `<td class=${columnClass}>2</td>`;
             document.querySelector(`.${rowClass}`).insertAdjacentHTML(`afterbegin`, data);
         }
-
-
     };
 };
+
+
 boardCreation();
 const snake = {
     head: [15, 16],
-
+    //movements left and right need to be cleaned up. a lot of rewritten code
+    moveLeft() {
+        document.getElementById('game').rows[snake.head[0]].cells[snake.head[1] - 1].classList.add('taken');
+        document.getElementById('game').rows[snake.head[0]].cells[snake.head[1] ].classList.remove('taken');
+        snake.head[1] -= 1;
+    },
+    moveRight(){
+        document.getElementById('game').rows[snake.head[0]].cells[snake.head[1] + 1].classList.add('taken');
+        document.getElementById('game').rows[snake.head[0]].cells[snake.head[1] ].classList.remove('taken');
+        snake.head[1] += 1;
+    },
 };
-
+//move snake.head around
 document.getElementById('game').rows[snake.head[0]].cells[snake.head[1]].classList.add('taken');
+document.addEventListener('keydown', function (e) {
+    console.log(e);
+    //move snake.head left
+    if (e.key === 'ArrowLeft') {
+        snake.moveLeft();
+        //     document.getElementById('game').rows[snake.head[0]].cells[snake.head[1]-1].classList.add('taken');
+    //     snake.head[1]-=1;
+    }else if(e.key==='ArrowRight'){
+        snake.moveRight();
+    }
+    
+    
+    
+
+});
