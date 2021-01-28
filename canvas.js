74ac0f20cf50660cb3ca8a82c92f95b4dfbd535e
ant <denicad@msn.com>
2021-01-28 17:18:28 -0500

    started using canvas but not quite sure how it works

---
+++ canvas.js
@@ -0,0 +1,31 @@
+const canvas=document.getElementById('game');
+const ctx=canvas.getContext('2d');
+ctx.fillStyle='blue';
+ctx.fillRect(0,0,400,400);
+const snake=[{x:200,y:200},{x:190,y:200},{x:180,y:200},{x:170,y:200},{x:160,y:200}];
+const drawSnakePart=(snakePart)=>{
+    ctx.fillStyle='green';
+    ctx.strokeStyle='darkgreen';
+    ctx.strokeRect(snakePart.x,snakePart.y,10,10);
+    ctx.fillRect(snakePart.x,snakePart.y,10,10);
+}
+const drawSnake=()=>snake.forEach(drawSnakePart);
+drawSnake();
+
+const moveSnake=()=>{
+    
+}
+
+const moveSnake=()=>{
+    const head={x:snake[0].x+dx,y:snake[0].y};
+    snake.unshift(head);
+    snake.pop();
+}
+
+document.addEventListener('keydown',function(e){
+    const left=37;
+    const right=39;
+    const up=38;
+    const down=40;
+    
+})
