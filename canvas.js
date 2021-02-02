15f5bd283e6486a4613bbc8a1f172604122cf20d
ant <denicad@msn.com>
2021-02-01 23:00:50 -0500

    working on boundary detection

--- canvas.js
+++ canvas.js
@@ -1,6 +1,6 @@
 const canvas=document.getElementById('game');
 const ctx=canvas.getContext('2d');
-let dx=-10;
+let dx=10;
 let dy=0;
 
 const clearBoard=()=>{
@@ -9,12 +9,31 @@ ctx.fillRect(0,0,400,400);
 };
 clearBoard();
 const snake=[{x:200,y:200},{x:190,y:200},{x:180,y:200},{x:170,y:200},{x:160,y:200}];
+const collision=()=>{
+    for (let i = 4; i< snake.length; i++) {
+        if(snake[i].x===snake[0].x||snake[i].y===snake[0].y){
+            return true;
+        }
+    }
+    const leftWall=snake[0].x<0;
+    const rightWall=snake[0].x>canvas.width-10;
+    const topWall=snake[0].y<0;
+    const bottomWall=snake[0].y>canvas.height-10;
+    return leftWall || rightWall|| topWall || bottomWall;
+    };
+
 const move=()=>{
-    setTimeout(function(){
+    
+    
+    setInterval(function(){
+        console.log(collision());
+        if (collision()) {
+            clearInterval(move());
+        }
         clearBoard();
         moveSnake();
         drawSnake();
-    },2000)
+    },200);
 }
 const drawSnakePart=(snakePart)=>{
     ctx.fillStyle='green';
@@ -46,12 +65,26 @@ document.addEventListener('keydown',function(e){
     const moveDown =dy===10;
     const key=e.keyCode;
     console.log(e);
-    if (key==left){
-        moveLeft;
+    if (key===left&&!moveRight){
+        dx=-10;
+        dy=0;
         move();
-        
     }
-
+    if (key===right&&!moveLeft) {
+        dx=10;
+        dy=0;
+        move();
+    }
+    if (key===up&&!moveDown) {
+        dx=0;
+        dy=-10;
+        move();
+    }
+    if(key===down&&!moveUp){
+        dx=0;
+        dy=10;
+        move();
+    }
     
     
 })
